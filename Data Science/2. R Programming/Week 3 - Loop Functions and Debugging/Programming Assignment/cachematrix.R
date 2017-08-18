e8a3662d6b48248fe5754102ea9f9984d2f96917
theodoreguo <jsguozitong@gmail.com>
2017-08-18 21:55:18 +0800

    Modified cachematrix.R

--- Data Science/2. R Programming/Week 3 - Loop Functions and Debugging/Programming Assignment/cachematrix.R
+++ Data Science/2. R Programming/Week 3 - Loop Functions and Debugging/Programming Assignment/cachematrix.R
@@ -3,6 +3,12 @@
 
 ## Write a short comment describing this function
 
+# makeCacheMatrix function is to create a special type of matrix, a cacheable matrix.
+# It takes a square matrix as an arguement and generates a list of operations: 
+# 1. Set the value of the matrix
+# 2. Get the value of the matrix
+# 3. Set the inverse of the matrix
+# 4. Get the inverse of the matrix
 makeCacheMatrix <- function(x = matrix()) {
     inverse <- NULL                                    
     set <- function(y) {
@@ -20,6 +26,11 @@ makeCacheMatrix <- function(x = matrix()) {
 
 ## Write a short comment describing this function
 
+# cacheSolve function calculates the inverse of the special "matrix" created with the above makeCacheMatrix function. 
+# Firstly, it checks if the inverse of the matrix has already been calculated. 
+# If so, it gets the inverse of the matrix from the cache and skips the computation. 
+# Otherwise, it calculates the inverse of the matrix of the data and sets the value 
+# of the inverse of the matrix in the cache using the setinverse function.
 cacheSolve <- function(x, ...) {
     ## Return a matrix that is the inverse of 'x'
 
